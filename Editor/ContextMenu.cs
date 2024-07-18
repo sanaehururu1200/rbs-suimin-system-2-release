4384040ae8e8714c18fef06911982547708a47dc
sanaehururu1200 <sanaehururu1200@gmail.com>
2024-07-19 08:16:46 +0900

    Update Prefab Serach Method

--- Editor/ContextMenu.cs
+++ Editor/ContextMenu.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 using UnityEditor;
+using System;
 
 namespace studio.raspberry.vrchat.ma_suimin_system
 {
     public static class ContextMenu
     {
         const string baseMenuPath = "GameObject/RBS-SuiminSystem/";
-        const string prefabPathBase = "Packages/studio.raspberry.vrchat.ma-suimin-system/Runtime/Prefabs/";
-        const string japanesePrefabName = "RBS_Suimin(日本語).prefab";
-        const string englishPrefabName = "RBS_Suimin(English).prefab";
+        const string japanesePrefabGuid = "99ff43deda213c145878efbecf96abf1";
+        const string englishPrefabGuid = "191431eacdb14f049bbc5ad19147b7fe";
 
         [MenuItem(baseMenuPath + "日本語版のセットアップ(Japanese)")]
         public static void ApplyToAvatar_Japanese(MenuCommand command)
         {
-            GameObject obj = UnityEditor.PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(prefabPathBase + japanesePrefabName)) as GameObject;
+            GameObject obj = UnityEditor.PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(japanesePrefabGuid))) as GameObject;
             if (obj != null)
             {
                 Transform objTransform = obj.transform;
@@ -26,7 +26,7 @@ namespace studio.raspberry.vrchat.ma_suimin_system
         [MenuItem(baseMenuPath + "Setup english version(英語)")]
         public static void ApplyToAvatar_English(MenuCommand command)
         {
-            GameObject obj = UnityEditor.PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(prefabPathBase + englishPrefabName)) as GameObject;
+            GameObject obj = UnityEditor.PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(englishPrefabGuid))) as GameObject;
             if (obj != null)
             {
                 Transform objTransform = obj.transform;
